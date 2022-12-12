2a53b58b6cb5fed223d8ac4c94fab8f95a7bc84e
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2022-12-12 15:01:13 +0100

    Added comments to day 12

--- days/day12/day12_1.c
+++ days/day12/day12_1.c
@@ -4,6 +4,10 @@
 #include "../../library/pointerList.h"
 #include "../../library/intList.h"
 
+/**
+ * A type for each cell, containing its position, height and
+ * the length of the shortest path to this cell from the start.
+ */
 typedef struct Cell {
     int x;
     int y;
@@ -11,16 +15,38 @@ typedef struct Cell {
     int shortest;
 } Cell;
 
+/**
+ * A linked list of cells for a queue.
+ */
 typedef struct QueueElement {
     Cell *cell;
     struct QueueElement *next;
 } QueueElement;
 
+/**
+ * Checks if a neighbour at position (cx; cy) is a valid next position and adds it to the queue if so.
+ * @param grid              A 2 dimensional list containing all cells.
+ * @param cx                The horizontal position of the neighbour.
+ * @param cy                The vertical position of the neighbour.
+ * @param required_height   The maximum height allowed for the neighbour.
+ * @param required_score    The score the neighbour will get.
+ * @param tail              A pointer to the tail of the queue.
+ */
 void addNeighbourCell(PointerList *grid, int cx, int cy, int required_height, int required_score, QueueElement **tail) {
+    /*
+     * Get the cell in the grid that corresponds with the position given. (the neighbour cell)
+     */
     Cell *cell = get_pointer(get_pointer(grid, cy), cx);
 
-    if (cell != NULL && cell->height <= required_height && cell->shortest > required_score) {
+    /*
+     * Check if the neighbour is valid according to the rules given.
+     */
+    if (cell != NULL && cell->height <= required_height && cell->shortest > 999999) {
 
+        /*
+         * Create a new queue element, update the shortest path length of the neighbour cell
+         * and add the neighbour queue element to the end of the queue.
+         */
         QueueElement *next = malloc(sizeof(QueueElement));
 
         cell->shortest = required_score;
@@ -65,7 +91,10 @@ int main() {
     /*
      * Make variables:
      *  line, which will store the line that was read from the file.
-     *  total_divider, which will keep track of the smallest common denominator.
+     *  grid, a 2-dimensional list containing all the cells in the input.
+     *  y, to keep track of the row in the input.
+     *  start_cell, to store the cell from where we start looking.
+     *  end_cell, to store the cell where we need to go.
      */
     char line[200];
 
@@ -81,19 +110,34 @@ int main() {
      */
     while (fgets(line, 200, file) != NULL) {
 
+        /*
+         * Create a list to go in the grid for the current line of the input.
+         */
         PointerList *current_line = initialize_pointerlist();
         add_pointer(grid, current_line);
 
+        /*
+         * Create variable x to keep track of the horizontal position of the cells to be added.
+         */
         int x = 0;
 
+        /*
+         * Loop over the input line.
+         */
         while (line[x] != '\n') {
 
+            /*
+             * Create a cell for the position.
+             */
             Cell *new_cell = malloc(sizeof(Cell));
             new_cell->x = x;
             new_cell->y = y;
             new_cell->shortest = 1000000000;
-            new_cell->height = line[x];
 
+            /*
+             * Set special values if the cell is either the start or end,
+             * else only set the height of the cell to the character.
+             */
             if (line[x] < '_') {
 
                 if (line[x] == 'S') {
@@ -108,8 +152,15 @@ int main() {
                     end_cell = new_cell;
 
                 }
+            } else {
+
+                new_cell->height = line[x];
+
             }
 
+            /*
+             * Add the row to the grid.
+             */
             add_pointer(current_line, new_cell);
             x++;
         }
@@ -117,23 +168,38 @@ int main() {
         y++;
     }
 
+    /*
+     * Create a queue by setting a head and tail to the same queue element of the starting cell.
+     */
     QueueElement *head = malloc(sizeof(QueueElement));
     QueueElement *tail = head;
 
     head->cell = start_cell;
     head->next = NULL;
 
+    /*
+     * Go through the queue of cells until the end cell is reached.
+     */
     while (1) {
 
+        /*
+         * Get the cell at the front of the queue.
+         */
         Cell *current = head->cell;
 
         int cx = current->x;
         int cy = current->y;
 
+        /*
+         * Check if we've reached the end.
+         */
         if (cx == end_cell->x && cy == end_cell->y) {
             break;
         }
 
+        /*
+         * Go through all the neighbours of the current cell, update them and add them to the queue.
+         */
         int required_score = current->shortest + 1;
         int required_height = current->height + 1;
 
@@ -142,13 +208,22 @@ int main() {
         addNeighbourCell(grid, cx - 1, cy, required_height, required_score, &tail);
         addNeighbourCell(grid, cx + 1, cy, required_height, required_score, &tail);
 
+        /*
+         * Free the current queue element and go to the next element in the queue.
+         */
         QueueElement *temp = head;
         head = head->next;
         free(temp);
     }
 
+    /*
+     * Print the length of the shortest path to the end cell.
+     */
     printf("%d\n", end_cell->shortest);
 
+    /*
+     * Clean up all the allocated memory.
+     */
     while (head != NULL) {
         QueueElement *temp = head;
         head = head->next;
--- days/day12/day12_2.c
+++ days/day12/day12_2.c
@@ -4,6 +4,10 @@
 #include "../../library/pointerList.h"
 #include "../../library/intList.h"
 
+/**
+ * A type for each cell, containing its position, height and
+ * the length of the shortest path to this cell from the start.
+ */
 typedef struct Cell {
     int x;
     int y;
@@ -11,16 +15,28 @@ typedef struct Cell {
     int shortest;
 } Cell;
 
+/**
+ * A linked list of cells for a queue.
+ */
 typedef struct QueueElement {
     Cell *cell;
     struct QueueElement *next;
 } QueueElement;
 
+/**
+ * Checks if a neighbour at position (cx; cy) is a valid next position and adds it to the queue if so.
+ * @param grid              A 2 dimensional list containing all cells.
+ * @param cx                The horizontal position of the neighbour.
+ * @param cy                The vertical position of the neighbour.
+ * @param required_height   The maximum height allowed for the neighbour.
+ * @param required_score    The score the neighbour will get.
+ * @param tail              A pointer to the tail of the queue.
+ */
 void addNeighbourCell(PointerList *grid, int cx, int cy, int required_height, int required_score, QueueElement **tail) {
 
     Cell *cell = get_pointer(get_pointer(grid, cy), cx);
 
-    if (cell != NULL && cell->height >= required_height && cell->shortest > required_score) {
+    if (cell != NULL && cell->height >= required_height && cell->shortest > 9999999) {
 
         QueueElement *next = malloc(sizeof(QueueElement));
 
@@ -65,7 +81,9 @@ int main() {
     /*
      * Make variables:
      *  line, which will store the line that was read from the file.
-     *  total_divider, which will keep track of the smallest common denominator.
+     *  grid, a 2-dimensional list containing all the cells in the input.
+     *  y, to keep track of the row in the input.
+     *  end_cell, to store the cell where we start.
      */
     char line[200];
 
@@ -80,20 +98,35 @@ int main() {
      */
     while (fgets(line, 200, file) != NULL) {
 
+        /*
+         * Create a list to go in the grid for the current line of the input.
+         */
         PointerList *current_line = initialize_pointerlist();
         add_pointer(grid, current_line);
 
+        /*
+         * Create variable x to keep track of the horizontal position of the cells to be added.
+         */
         int x = 0;
 
+        /*
+         * Loop over the input line.
+         */
         while (line[x] != '\n') {
 
+            /*
+             * Create a cell for the position.
+             */
             Cell *new_cell = malloc(sizeof(Cell));
 
             new_cell->x = x;
             new_cell->y = y;
             new_cell->shortest = 1000000000;
-            new_cell->height = line[x];
 
+            /*
+             * Set special values if the cell is either the start or end,
+             * else only set the height of the cell to the character.
+             */
             if (line[x] < '_') {
 
                 if (line[x] == 'S') {
@@ -107,8 +140,15 @@ int main() {
                     end_cell = new_cell;
 
                 }
+            } else {
+
+                new_cell->height = line[x];
+
             }
 
+            /*
+             * Add the row to the grid.
+             */
             add_pointer(current_line, new_cell);
             x++;
         }
@@ -116,22 +156,38 @@ int main() {
         y++;
     }
 
+    /*
+     * Create a queue by setting a head and tail to the same queue element of the starting cell.
+     */
     QueueElement *head = malloc(sizeof(QueueElement));
     QueueElement *tail = head;
 
     head->cell = end_cell;
     head->next = NULL;
 
+    /*
+     * Go through the queue of cells until the end cell is reached.
+     */
     while (1) {
 
+        /*
+         * Get the cell at the front of the queue.
+         */
         Cell *current = head->cell;
+
         int cx = current->x;
         int cy = current->y;
 
+        /*
+         * Check if we've reached the end.
+         */
         if (current->height == 'a') {
             break;
         }
 
+        /*
+         * Go through all the neighbours of the current cell, update them and add them to the queue.
+         */
         int required_score = current->shortest + 1;
         int required_height = current->height - 1;
 
@@ -140,14 +196,23 @@ int main() {
         addNeighbourCell(grid, cx - 1, cy, required_height, required_score, &tail);
         addNeighbourCell(grid, cx + 1, cy, required_height, required_score, &tail);
 
+        /*
+         * Free the current queue element and go to the next element in the queue.
+         */
         QueueElement *temp = head;
         head = head->next;
         free(temp);
 
     }
 
+    /*
+     * Print the length of the shortest path to the end cell.
+     */
     printf("%d\n", head->cell->shortest);
 
+    /*
+     * Clean up all the allocated memory.
+     */
     while (head != NULL) {
         QueueElement *temp = head;
         head = head->next;
@@ -159,6 +224,8 @@ int main() {
     }
 
     delete_pointerlist_not_pointers(grid);
+
+    fclose(file);
 
     /*
      * Close the timer and print the taken time.
