dee725717979c203c07f3e815b026cccbe0cc870
Laurens de Swart <L.j.p.deSwart@student.tudelft.nl>
2022-12-17 23:20:20 +0100

    Day 16 Part 1

--- days/day16/day16_1.c
+++ days/day16/day16_1.c
@@ -8,19 +8,35 @@
 #define SECONDS 30
 
 typedef struct Node {
-    int id;
     int flow_rate;
     PointerList *edges;
-    PointerList *best;
 } Node;
+
+typedef struct NodeQueueElement {
+    int node_id;
+    int total;
+    struct NodeQueueElement *next;
+} NodeQueueElement;
 
 typedef struct QueueElement {
     int node_id;
+    int total;
     int seconds_left;
-    int nodes_activated;
-    PointerList *activated_nodes;
+    int *visited_nodes;
     struct QueueElement *next;
 } QueueElement;
+
+void reset_visited(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = 0;
+    }
+}
+
+void copy_into_array(const int *source, int *dest, int size) {
+    for (int i = 0; i < size; i++) {
+        dest[i] = source[i];
+    }
+}
 
 /*
  * Day 16, Part 1
@@ -40,7 +56,7 @@ int main() {
 
     FILE *file;
 
-    file = fopen("../days/day16/testInput_day16.txt", "r");
+    file = fopen("../days/day16/input_day16.txt", "r");
 
     /*
      * Shutdown program if the file can't be found or another error occurred.
@@ -60,14 +76,12 @@ int main() {
      * Loop over the entire input.
      */
     int id = 0;
+    int start_index;
 
     while (fgets(line, 75, file) != NULL) {
         Node *current_node = malloc(sizeof(Node));
 
         add_pointer(nodes, current_node);
-
-        current_node->id = id;
-        id++;
 
         char *ptr = line + 6;
         char *name = malloc(sizeof(char) * 3);
@@ -75,6 +89,11 @@ int main() {
         name[1] = ptr[1];
         name[2] = '\0';
         add_pointer(names, name);
+
+        if (strcmp(name, "AA") == 0) {
+            start_index = id;
+        }
+        id++;
 
         ptr += 17;
 
@@ -96,14 +115,6 @@ int main() {
         }
 
         add_pointer(outgoing_edge_names, outgoing_edges);
-
-        PointerList *best = initialize_pointerlist_of_capacity(SECONDS);
-
-        for (int i = 0; i < SECONDS; i++) {
-            add_int(best, 0);
-        }
-
-        current_node->best = best;
     }
 
     for (int i = 0; i < nodes->size; i++) {
@@ -133,7 +144,109 @@ int main() {
 
     delete_pointerlist(names);
 
-    print_ints(((Node *) get_pointer(nodes, 0))->edges);
+    int ** distances = malloc(sizeof(int *) * nodes->size);
+
+    for (int i = 0; i < nodes->size; i++) {
+        distances[i] = malloc(sizeof(int) * nodes->size);
+
+        int visited[nodes->size];
+        reset_visited(visited, nodes->size);
+        visited[i] = 1;
+
+        NodeQueueElement *head = malloc(sizeof(NodeQueueElement));
+        head->node_id = i;
+        head->total = 0;
+        head->next = NULL;
+        NodeQueueElement *tail = head;
+
+        while (head != NULL) {
+            NodeQueueElement *current = head;
+            distances[i][current->node_id] = current->total;
+
+            PointerList *edges = ((Node *) get_pointer(nodes, current->node_id))->edges;
+            for (int j = 0; j < edges->size; j++) {
+                int current_edge = get_int(edges, j);
+                if (visited[current_edge] == 0) {
+                    NodeQueueElement *new_element = malloc(sizeof(NodeQueueElement));
+                    new_element->node_id = current_edge;
+                    new_element->total = current->total + 1;
+                    new_element->next = NULL;
+
+                    visited[current_edge] = 1;
+
+                    tail->next = new_element;
+                    tail = tail->next;
+                }
+            }
+
+            head = head->next;
+            free(current);
+        }
+    }
+
+    QueueElement *head = malloc(sizeof(QueueElement));
+    head->node_id = start_index;
+    head->seconds_left = SECONDS + 1;
+    head->total = 0;
+    head->next = NULL;
+    head->visited_nodes = malloc(sizeof(int) * nodes->size);
+
+    for (int i = 0; i < nodes->size; i++) {
+        if (((Node *) get_pointer(nodes, i))->flow_rate == 0) {
+            head->visited_nodes[i] = 1;
+        } else {
+            head->visited_nodes[i] = 0;
+        }
+    }
+
+    QueueElement *tail = head;
+
+    int maximum_pressure = 0;
+
+    while (head != NULL) {
+        QueueElement *current_element = head;
+        current_element->seconds_left--;
+        int *current_visited = current_element->visited_nodes;
+        Node *current_node = get_pointer(nodes, current_element->node_id);
+        current_element->total += current_node->flow_rate * current_element->seconds_left;
+        int could_add = 0;
+        for (int i = 0; i < nodes->size; i++) {
+            int distance = distances[current_element->node_id][i];
+            if (current_visited[i] == 0 && current_element->seconds_left - distance - 1 >= 0) {
+                could_add = 1;
+                QueueElement *new_element = malloc(sizeof(QueueElement) + 1);
+                new_element->visited_nodes = malloc(sizeof(int) * nodes->size);
+                copy_into_array(current_element->visited_nodes, new_element->visited_nodes, nodes->size);
+                new_element->visited_nodes[i] = 1;
+                new_element->seconds_left = current_element->seconds_left - distance;
+                new_element->node_id = i;
+                new_element->total = current_element->total;
+                new_element->next = NULL;
+
+                tail->next = new_element;
+                tail = tail->next;
+            }
+        }
+        if (!could_add && current_element->total > maximum_pressure) {
+            maximum_pressure = current_element->total;
+        }
+
+        head = head->next;
+        free(current_element->visited_nodes);
+        free(current_element);
+    }
+
+    printf("%d\n", maximum_pressure);
+
+    for (int i = 0; i < nodes->size; i++) {
+        delete_pointerlist(((Node *) get_pointer(nodes, i))->edges);
+        free(distances[i]);
+    }
+
+    free(distances);
+
+
+    delete_pointerlist(nodes);
 
     /*
     * Close the timer and print the taken time.
